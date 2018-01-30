6c9687fed33b5f788039fdd984246ad9cd6a43fb
Balveer Singh <32636670+BalveerIYC@users.noreply.github.com>
2018-01-30 07:02:28 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+		printf("Number is positive");
+                if(n<0)
+		printf("Number is negative");
+	else
+		printf("Number is Zero");
+
+	}
